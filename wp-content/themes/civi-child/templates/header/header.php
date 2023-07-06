575610ec96640660c183ef30b1deac99ff8ca432
tuncadev <tunca.development@gmail.com>
2023-07-06 22:21:45 +0300

    hamburger to right test

--- wp-content/themes/civi-child/templates/header/header.php
+++ wp-content/themes/civi-child/templates/header/header.php
@@ -2,7 +2,7 @@
 	<div class="row">
 
 		<div class="left-header">
-			<?php echo Civi_Templates::canvas_menu(); ?>
+
 			<?php echo Civi_Templates::style_logo(); ?>
 
 			<div class="d-none d-xl-block">
@@ -20,7 +20,7 @@
 			<div class="d-none d-xl-block">
 				<?php echo Civi_Templates::account(); ?>
 			</div>
-
+			<?php echo Civi_Templates::canvas_menu(); ?>
 			<div class="d-xl-none">
 				<?php echo Civi_Templates::search_icon('icon', true); ?>
 			</div>
