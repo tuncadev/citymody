c080549799400e5b4869c354b21e8908fd964dfd
tuncadev <tunca.development@gmail.com>
2023-07-15 05:09:55 +0300

    404

--- wp-content/themes/civi-child/404.php
+++ wp-content/themes/civi-child/404.php
@@ -15,7 +15,7 @@ get_header();
 			<div class="area-404 align-center">
 				<h2><?php esc_html_e('Hmm, that didn’t work', 'civichild'); ?></h2>
 				<p><?php esc_html_e("Sorry, we couldn't find the page you were looking for. The page you are looking for may be moved, removed, renamed or never found.", 'civichild'); ?></p>
-				<img src="https://www.citymody.com/wp-content/uploads/2023/07/404.png" alt="<?php esc_attr_e('404', 'civichild'); ?>">
+				<img src="https://www.citymody.com/wp-content/uploads/2023/07/404.png" alt="<?php esc_attr_e('404', 'civichild'); ?>" style="max-width: 500px">
 				<a class="civi-button button-outline-accent button-icon-right" href="<?php echo esc_url(home_url()); ?>">
 					<?php esc_html_e('Go to home page', 'civichild') ?>
 					<i class="fas fa-chevron-right"></i>
