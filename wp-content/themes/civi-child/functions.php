a1b6d11c746f98f2c4620dbd1a8a619678839a52
tuncadev <tunca.development@gmail.com>
2023-07-15 19:33:00 +0300

    wp die

--- wp-content/themes/civi-child/functions.php
+++ wp-content/themes/civi-child/functions.php
@@ -223,7 +223,43 @@ function list_my_childs($parent_id, $taxonomyName) {
 }
 
 
+/**
+ * Maybe change the wp_die_handler.
+ */
+add_filter( 'wp_die_handler', function( $handler ) {
+	return ! is_admin() ? 'themed_wp_die_handler' : $handler;
+}, 10 );
 
+
+/**
+ * Use a custom wp_die() handler.
+ */
+function themed_wp_die_handler( $message, $title = '', $args = array() ) {
+	$defaults = array( 'response' => 500 );
+	$r = wp_parse_args($args, $defaults);
+
+	if ( function_exists( 'is_wp_error' ) && is_wp_error( $message ) ) {
+		$errors = $message->get_error_messages();
+		switch ( count( $errors ) ) {
+			case 0 :
+				$message = '';
+				break;
+			case 1 :
+				$message = $errors[0];
+				break;
+			default :
+				$message = "<ul>\n\t\t<li>" . join( "</li>\n\t\t<li>", $errors ) . "</li>\n\t</ul>";
+				break;
+		}
+
+	} else {
+		$message = strip_tags( $message );
+	}
+
+	require_once get_stylesheet_directory() . '/wp-die.php';
+
+	die();
+}
 
 
 
