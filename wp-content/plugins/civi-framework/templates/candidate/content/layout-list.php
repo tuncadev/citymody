7aaa09ac373dd7858e0d5df30f0dadf303fb6e2c
tuncadev <tunca.development@gmail.com>
2023-07-28 22:52:22 +0300

    more less

--- wp-content/plugins/civi-framework/templates/candidate/content/layout-list.php
+++ wp-content/plugins/civi-framework/templates/candidate/content/layout-list.php
@@ -31,9 +31,16 @@ $enable_candidate_des = civi_get_option('enable_candidate_show_des');
 	const hidden= el.parentElement.querySelectorAll(".hidden");
 	el.addEventListener("click", ()=>{
 	 hidden.forEach(h=> h.classList.toggle("hidden")) 
-	 if (hidden[0].classList.contains("hidden"))
-			 el.innerHTML = "<?php echo __("Show more" , "civi-framework"); ?>";
-		 else el.innerHTML = "<?php echo __("Show less" , "civi-framework"); ?>";
+	 if (hidden[0].classList.contains("hidden")) {
+	 		el.classList.remove("up");
+	 		el.classList.add("down");
+			el.innerHTML = "<?php echo __("Show more" , "civi-framework"); ?>";
+		}
+		else { 
+			el.classList.remove("down");
+			el.classList.add("up");
+			el.innerHTML = "<?php echo __("Show less" , "civi-framework"); ?>"; 
+		}
 	});
  });
 </script>
