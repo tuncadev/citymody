93dee61164e03f4becfd218911e2a9171fa4f57a
tuncadev <tunca.development@gmail.com>
2023-07-28 22:23:08 +0300

    more less

--- wp-content/plugins/civi-framework/templates/candidate/content/layout-list.php
+++ wp-content/plugins/civi-framework/templates/candidate/content/layout-list.php
@@ -135,7 +135,7 @@ $enable_candidate_des = civi_get_option('enable_candidate_show_des');
                     </a>
                 <?php } ?>
 								<br><?php if($i > 4) { ?>
-								<a class="acc-more" ><?php echo __("Show more" , "civi-framework"); ?></a>
+								<a class="acc-more" ><?php echo __("Show " . $i - 4 . " more" , "civi-framework"); ?></a>
 								<?php } ?>
             </div>
         <?php } ?>
