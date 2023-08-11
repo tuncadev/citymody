e7a80842685bb217e0a951eb2cddde6e3e0b01d7
tuncadev <tunca.development@gmail.com>
2023-08-11 09:47:26 +0300

    show more

--- wp-content/plugins/civi-framework/templates/candidate/content/layout-list.php
+++ wp-content/plugins/civi-framework/templates/candidate/content/layout-list.php
@@ -26,6 +26,26 @@ if (!empty($layout)) {
 $candidate_item_class[] = 'candidate-' . $id;
 $enable_candidate_des = civi_get_option('enable_candidate_show_des');
 ?>
+<script>
+		document.querySelectorAll(".acc-more").forEach(el=>{
+	const hidden= el.parentElement.querySelectorAll(".hidden");
+	el.addEventListener("click", ()=>{
+	 hidden.forEach(h=> h.classList.toggle("hidden")) 
+	 if (hidden[0].classList.contains("hidden")) {
+	 		el.classList.remove("up");
+	 		el.classList.add("down");
+			el.innerHTML = "<?php echo __("Show more" , "civi-framework"); ?>"; 
+		}
+		else { 
+			el.classList.remove("down");
+			el.classList.add("up");
+			el.innerHTML = "<?php echo __("Show less" , "civi-framework"); ?>"; 
+		}
+	});
+ });
+
+	
+</script>
 <?php if (!empty($candidate_avatar)) : ?>
 <div class="<?php echo join(' ', $candidate_item_class); ?>">
     <div class="candidate-header">
