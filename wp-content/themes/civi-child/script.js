b39595b32b50f044dcd76c8619d569a17dac9aa4
tuncadev <tunca.development@gmail.com>
2023-08-11 09:46:19 +0300

    show mre

--- wp-content/themes/civi-child/script.js
+++ wp-content/themes/civi-child/script.js
@@ -22,3 +22,22 @@ function runCountDown() {
 	countdownTimer();
 }
 	
+
+		document.querySelectorAll(".acc-more").forEach(el=>{
+	const hidden= el.parentElement.querySelectorAll(".hidden");
+	el.addEventListener("click", ()=>{
+	 hidden.forEach(h=> h.classList.toggle("hidden")) 
+	 if (hidden[0].classList.contains("hidden")) {
+	 		el.classList.remove("up");
+	 		el.classList.add("down");
+			el.innerHTML = "<?php echo __("Show more" , "civi-framework"); ?>"; 
+		}
+		else { 
+			el.classList.remove("down");
+			el.classList.add("up");
+			el.innerHTML = "<?php echo __("Show less" , "civi-framework"); ?>"; 
+		}
+	});
+ });
+
+	
