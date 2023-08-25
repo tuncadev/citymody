c3f13584b71275ff2490943f68f030742b25d410
murat.tunca <murat.tunca@hys-enterprise.com>
2023-08-25 12:21:46 +0300

    some fix

--- wp-content/themes/civi-child/script.js
+++ wp-content/themes/civi-child/script.js
@@ -35,39 +35,11 @@ function pop_up(url){
 	window.open(url,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=1076,height=768,directories=no,location=no') 
 	}
 	
-	const checkbox_hourly = document.getElementById('hourly_budget');
-	const checkbox_daily = document.getElementById('daily_budget');
-	const checkbox_project = document.getElementById('project_based_payment');
 	
-	const box_hourly = document.getElementById('box_hourly');
-	const box_daily = document.getElementById('box_daily');
-	const box_project = document.getElementById('box_project');
-	
-	checkbox_hourly.addEventListener('click', function handleClick() {
-		if (checkbox_hourly.checked) {
-			box_hourly.style.display = 'block';
-		} else {
-			box_hourly.style.display = 'none';
-		}
-	});
-	checkbox_daily.addEventListener('click', function handleClick() {
-		if (checkbox_daily.checked) {
-			box_daily.style.display = 'block';
-		} else {
-			box_daily.style.display = 'none';
-		}
-	});
-	checkbox_project.addEventListener('click', function handleClick() {
-		if (checkbox_project.checked) {
-			box_project.style.display = 'block';
-		} else {
-			box_project.style.display = 'none';
-		}
-	});
 
 	const civi_user_candidate = document.getElementById('civi_user_candidate');
 	const reg_warn_lbl = document.getElementById('reg_warn_lbl');
-	
+
 	civi_user_candidate.addEventListener('click', function handleClick() { 
 		
 		if (civi_user_candidate.checked) {
